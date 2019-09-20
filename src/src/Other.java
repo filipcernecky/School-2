be425b69c50cfd5891ca3a57e31951f999560bd8
cerneckyf <filip.cernecky@akademiasovy.sk>
2019-09-20 13:20:18 +0200

    .

--- src/src/Other.java
+++ src/src/Other.java
@@ -8,4 +8,19 @@ public class Other {
 
         return Pattern.matches("^(([0][.|,][0-9]{2})|([1-9]+([.|,][0-9]{2})?))([\\s]?(€|EUR|eur))?$", eur);
     }
+    //WIP
+    /*
+   String str = "test";
+		byte[] byteArr = str.getBytes();
+		System.out.println("String to byte array: " + Arrays.toString(byteArr));
+    */
+     
+    public char crypt(char input){
+        byte part1 = (byte);
+        byte part2 = (byte);
+ 
+        int result = (byte) (part1|part2);
+
+        return (char);
+    }
 }
